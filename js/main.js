8bed63e61ab38956a3e2bdd895d4fb4307b6396e
Jason-Donmoyer <j.donmoyer523@gmail.com>
2020-08-12 04:53:28 -0400

    added changeBackground function

--- js/main.js
+++ js/main.js
@@ -1,22 +1,26 @@
 const body = document.querySelector('body');
 
-// body.onload(getStartTime);
 // Clock Functions
-
 
 function getStartTime() {
   var today = new Date();
   var hour = today.getHours();
   var minutes = today.getMinutes();
   var seconds = today.getSeconds();
+  var mornOrNoon = amOrPm(hour);
   hour = checkCurrentTime(checkCurrentHour(hour));
   minutes = checkCurrentTime(minutes);
   seconds = checkCurrentTime(seconds);
 
-  document.querySelector('#clock').innerHTML = hour + ':' + minutes + ':' + seconds;
-  var timeOut = setTimeout(getStartTime, 500);
+
+  document.querySelector('#clock').innerHTML = hour + ':' + minutes + ':' + seconds + mornOrNoon;
+  setTimeout(getStartTime, 500);
+
+  changeBackgroundImage(hour);
 
 }
+
+// check if past noon
 
 function checkCurrentHour(index) {
   if (index > 12) {
@@ -25,9 +29,34 @@ function checkCurrentHour(index) {
   return index;
 }
 
+// adds zeros to single digits
+
 function checkCurrentTime(index) {
   if (index < 10) {
     index = "0" + index;
   }
   return index;
+}
+
+// displays am or pm
+
+function amOrPm(hour) {
+  if (hour >= 12) {
+    mornOrNoon = 'pm';
+  } else {
+    mornOrNoon = 'am';
+  }
+  return mornOrNoon;
+}
+
+
+// Change Background Function
+
+const mainBackground = document.querySelector('main');
+
+function changeBackgroundImage(hour) {
+  if (hour > 17 || hour < 5) {
+    mainBackground.style.backgroundImage = 'url(images/dark_ocean_view.jpg)';
+    mainBackground.style.color = '#FFF';
+  }
 }
